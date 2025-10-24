9379638c27768f67408be9b74628f1a4577ea607
rawanamrrr <rawanamr20002@icloud.com>
2025-10-24 21:05:15 +0300

    app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,30 +18,28 @@ const playfair = Playfair_Display({
 })
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://zeyadandrawan.netlify.app'),
-  title: "Zeyad & Rawan - Save the Date",
-  description: "You are invited to the engagement of Zeyad & Rawan | Friday, November 7, 2025 at 07:00 PM | Diva Garden Hall, Talkha City | Join us as we begin our journey of love and togetherness",
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'https://localhost:3000'),
+  title: "Welcome to Our Beginning",
+  description: "Celebrating the start of our journey together",
   generator: "Digitiva",
   openGraph: {
-    title: "Zeyad & Rawan - Save the Date",
-    description: "You are invited to the engagement of Zeyad & Rawan | Friday, November 7, 2025 at 07:00 PM | Diva Garden Hall, Talkha City",
+    title: "Welcome to Our Beginning",
+    description: "Celebrating the start of our journey together",
     images: [
       {
-        url: "https://zeyadandrawan.netlify.app/invitation-design.png",
-        width: 768,
-        height: 1365,
-        alt: "Zeyad & Rawan Engagement Invitation - Save the Date",
+        url: "/invitation-design.png",
+        width: 1200,
+        height: 630,
+        alt: "Our Engagement Invitation",
       },
     ],
     type: "website",
-    siteName: "Zeyad & Rawan Engagement",
-    locale: "en_US",
   },
   twitter: {
     card: "summary_large_image",
-    title: "Zeyad & Rawan - Save the Date",
-    description: "You are invited to the engagement of Zeyad & Rawan | Friday, November 7, 2025 at 07:00 PM | Diva Garden Hall, Talkha City",
-    images: ["https://zeyadandrawan.netlify.app/invitation-design.png"],
+    title: "Welcome to Our Beginning",
+    description: "Celebrating the start of our journey together",
+    images: ["/invitation-design.png"],
   },
   icons: {
     icon: "/invitation-design.png",
